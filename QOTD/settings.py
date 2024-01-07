43e041d164ce96878f561e90bbda4f1a2338c89c
haokai-xuan <haokai.xuan2006@gmail.com>
2024-01-06 20:52:41 -0400

    Second commit

--- QOTD/settings.py
+++ QOTD/settings.py
@@ -124,10 +124,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+    os.path.join(BASE_DIR, 'media'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
